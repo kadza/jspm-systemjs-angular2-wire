cfa73d2c6db6d4562abd558fdd0106ec7a6525c4
Kujawiak <lkujawia@lod-mlkujawia.local>
2016-02-27 18:29:46 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+gulp.task('play', function () {
+    var http = require('http');
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+    var open = require('open');
+
+    var port = 9000, app;
+
+    app = connect().use(serveStatic(__dirname));
+    http.createServer(app).listen(port, function () {
+        open('http://localhost:' + port);
+    });
+});
+
+gulp.task('default', ['play']);
